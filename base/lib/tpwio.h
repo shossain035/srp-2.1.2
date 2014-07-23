7605ca7da46caba9e17e70f4ee618d207639feac
shossain035 <nahid@lithouse.co>
2014-07-22 19:28:28 -0700

    Revert "modified for raspberry pi"
    This reverts commit 1fe2fd2b31074331946bc24ab042b200ce82c22f.

--- base/lib/tpwio.h
+++
@@ -1,3 +0,0 @@
-extern int tpw_lock P_((void));
-extern int tpw_lock_first P_((void));
-extern int tpw_unlock P_((void));
--- java/1.1.x/security/srp/NoSuchUserException.java
+++
@@ -1,7 +0,0 @@
-package security.srp;
-
-public class NoSuchUserException extends Exception {
-  public NoSuchUserException() { super(); }
-
-  public NoSuchUserException(String arg) { super(arg); }
-}
